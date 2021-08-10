078fb15093342461b8afd2ac104b48d7a359132f
PARAVPREET17 <psingh9_be20@thapar.edu>
2021-08-11 02:49:14 +0530

    Third commit

--- WebPr/settings.py
+++ WebPr/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages',
+    'listings',
+    'realtors',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +79,11 @@ WSGI_APPLICATION = 'WebPr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PractiseProject',
+        'USER':'postgres',
+        'PASSWORD': '21may2021',
+        'HOST': 'localhost'
     }
 }
 
---
+++ listings/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns =[ 
+    path('',views.index,name='listings'),
+    path('<int:listing_id>',views.listing,name='listing'),#To capture a value from the URL, use angle brackets.
+    path('search',views.search,name='searchlisting'),
+]
