ab7d2dd57d9adb3498f39c20644fae4e199cb5bf
PARAVPREET17 <psingh9_be20@thapar.edu>
2021-08-12 16:14:45 +0530

    updated admin page

--- WebPr/settings.py
+++ WebPr/settings.py
@@ -134,3 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Media Folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
