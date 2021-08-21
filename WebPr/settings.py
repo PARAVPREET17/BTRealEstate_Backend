3f27e61c475cff158157ec510cbdd838f897d7d0
PARAVPREET17 <psingh9_be20@thapar.edu>
2021-08-21 19:57:09 +0530

    changes done

--- WebPr/settings.py
+++ WebPr/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'pages',
     'listings',
     'realtors',
@@ -139,3 +140,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Media Folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS={
+    messages.ERROR:'danger'
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,7 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+setTimeout(function() {{
+    $('#message').fadeOut('slow');
+}},3000); 
+// fadeout error message after few seconds automatically
